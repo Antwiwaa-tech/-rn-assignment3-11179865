c02154f9bdf0923c77166d328fc7f4801ef6cec3
Antwiwaa <nanaafuaantwiwaa624@gmail.com>
2024-05-31 18:26:12 +0000

    Created Expo and changed background color ,added writing and the search bar

---
+++ MyApp/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, View, Image, ScrollView } from 'react-native';
+
+
+const Picture = require('./assets/Frame 1.png');
+const Search = require('./assets/Search.png');
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={styles.content}>
+          <Image source={Picture} style={styles.mainImage} />
+          <View style={styles.searchContainer}>
+            <Image source={Search} />
+          </View>
+
+          
+        </View>
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f7f0e8',
+  },
+  content: {
+    marginLeft: 25,
+    marginRight: 12,
+  },
+  mainImage: {
+    marginBottom: 30,
+  },
+  searchContainer: {
+    alignItems: 'center',
+  },
+});
+
